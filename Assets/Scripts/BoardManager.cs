386639778705e199ec7afc771ce1d249d5216362
Phil Flavin <pflavin@gmail.com>
2019-01-08 09:27:50 +0000

    Local comott

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -93,7 +93,7 @@ public class BoardManager : Singleton<BoardManager>
                 else
                 {
                     int i = 0;//loop counter
-                    //pick a random tile to place here
+                    //pick a random tile to place here ....
                     while (!CheckNeighbours(newTile.GetComponent<TileScript>(), currentPoint))
                     {
                         newTile= drawTile();
